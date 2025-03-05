8b7d16414c459fbd577c33292730f3eb79ae526e
pierreemad93 <pierreemad93@outlook.com>
2025-03-05 16:09:01 +0200

    redirect login page to admin dashboard

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="wrapper vh-100">
         <div class="row align-items-center h-100">
-            <form class="col-lg-3 col-md-4 col-10 mx-auto text-center">
+            <form class="col-lg-3 col-md-4 col-10 mx-auto text-center" method="POST" action="{{ route('admin.login') }}">
+                @csrf
                 <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
                     <svg version="1.1" id="logo" class="navbar-brand-img brand-md" xmlns="http://www.w3.org/2000/svg"
                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120"
@@ -16,15 +17,21 @@
                     </svg>
                 </a>
                 <h1 class="h6 mb-3">Sign in</h1>
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
                 <div class="form-group">
                     <label for="inputEmail" class="sr-only">Email address</label>
                     <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Email address"
-                        required="" autofocus="">
+                        name="email" :value="old('email')">
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
                 <div class="form-group">
                     <label for="inputPassword" class="sr-only">Password</label>
                     <input type="password" id="inputPassword" class="form-control form-control-lg" placeholder="Password"
-                        required="">
+                         name="password">
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
                 </div>
                 <div class="checkbox mb-3">
                     <label>
--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::view('/', 'admin.index');
-    Route::view('/login', 'admin.auth.login')->name('login');
 });
 
 require __DIR__ . '/auth.php';
